44fcb1055bd5553f5fb14be25193158cbbe124f9
danquxunhuan <danqu130@users.noreply.github.com>
2018-05-10 04:44:07 +0800

    first commit: implemented

---
+++ test.py
@@ -0,0 +1,170 @@
+# coding: utf-8
+
+# # Define Dependencies
+
+# In[1]:
+
+
+# Import the necessary libraries
+from sklearn.decomposition import PCA
+import os
+import scipy.io as sio
+import numpy as np
+from keras.models import load_model
+from keras.utils import np_utils
+from sklearn.metrics import classification_report, confusion_matrix
+import itertools
+import spectral
+
+# In[2]:
+
+
+# Global Variables
+windowSize = 5
+numPCAcomponents = 30
+testRatio = 0.25
+
+
+# # Define the neccesary functions for later use
+
+# In[3]:
+
+
+def loadIndianPinesData():
+    data_path = os.path.join(os.getcwd(), 'data')
+    data = sio.loadmat(os.path.join(data_path, 'Indian_pines.mat'))['indian_pines']
+    labels = sio.loadmat(os.path.join(data_path, 'Indian_pines_gt.mat'))['indian_pines_gt']
+
+    return data, labels
+
+
+def reports(X_test, y_test):
+    Y_pred = model.predict(X_test)
+    y_pred = np.argmax(Y_pred, axis=1)
+    target_names = ['Alfalfa', 'Corn-notill', 'Corn-mintill', 'Corn',
+                    'Grass-pasture', 'Grass-trees', 'Grass-pasture-mowed',
+                    'Hay-windrowed', 'Oats', 'Soybean-notill', 'Soybean-mintill',
+                    'Soybean-clean', 'Wheat', 'Woods', 'Buildings-Grass-Trees-Drives',
+                    'Stone-Steel-Towers']
+
+    classification = classification_report(np.argmax(y_test, axis=1), y_pred, target_names=target_names)
+    confusion = confusion_matrix(np.argmax(y_test, axis=1), y_pred)
+    score = model.evaluate(X_test, y_test, batch_size=32)
+    Test_Loss = score[0] * 100
+    Test_accuracy = score[1] * 100
+
+    return classification, confusion, Test_Loss, Test_accuracy
+
+
+def applyPCA(X, numComponents=75):
+    newX = np.reshape(X, (-1, X.shape[2]))
+    pca = PCA(n_components=numComponents, whiten=True)
+    newX = pca.fit_transform(newX)
+    newX = np.reshape(newX, (X.shape[0], X.shape[1], numComponents))
+    return newX, pca
+
+
+def Patch(data, height_index, width_index):
+    # transpose_array = data.transpose((2,0,1))
+    # print transpose_array.shape
+    height_slice = slice(height_index, height_index + PATCH_SIZE)
+    width_slice = slice(width_index, width_index + PATCH_SIZE)
+    patch = data[height_slice, width_slice, :]
+
+    return patch
+
+
+# In[4]:
+
+
+PATH = os.getcwd()
+print(PATH)
+
+# In[5]:
+
+
+X_test = np.load("./data/XtestWindowSize"
+                 + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(testRatio) + ".npy")
+y_test = np.load("./data/ytestWindowSize"
+                 + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(testRatio) + ".npy")
+
+# In[6]:
+
+
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[3], X_test.shape[1], X_test.shape[2]))
+y_test = np_utils.to_categorical(y_test)
+
+# In[7]:
+
+
+# load the model architecture and weights
+model = load_model('./model/my_model_epochs30.h5')
+
+# In[8]:
+
+
+classification, confusion, Test_loss, Test_accuracy = reports(X_test, y_test)
+classification = str(classification)
+confusion = str(confusion)
+file_name = './result/report' + "WindowSize" + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(
+    testRatio) + ".txt"
+with open(file_name, 'w') as x_file:
+    x_file.write('{} Test loss (%)'.format(Test_loss))
+    x_file.write('\n')
+    x_file.write('{} Test accuracy (%)'.format(Test_accuracy))
+    x_file.write('\n')
+    x_file.write('\n')
+    x_file.write('{}'.format(classification))
+    x_file.write('\n')
+    x_file.write('{}'.format(confusion))
+
+# In[9]:
+
+
+# load the original image
+X, y = loadIndianPinesData()
+
+# In[10]:
+
+
+X, pca = applyPCA(X, numComponents=numPCAcomponents)
+
+# In[11]:
+
+
+height = y.shape[0]
+width = y.shape[1]
+PATCH_SIZE = 5
+numComponents = 30
+
+# In[13]:
+
+
+# calculate the predicted image
+outputs = np.zeros((height, width))
+for i in range(height - PATCH_SIZE + 1):
+    for j in range(width - PATCH_SIZE + 1):
+        p = int(PATCH_SIZE / 2)
+        # print(y[i+p][j+p])
+        # target = int(y[i+PATCH_SIZE/2, j+PATCH_SIZE/2])
+        target = y[i + p][j + p]
+        if target == 0:
+            continue
+        else:
+            image_patch = Patch(X, i, j)
+            # print (image_patch.shape)
+            X_test_image = image_patch.reshape(1, image_patch.shape[2], image_patch.shape[0],
+                                               image_patch.shape[1]).astype('float32')
+            prediction = (model.predict_classes(X_test_image))
+            outputs[i + p][j + p] = prediction + 1
+
+# In[15]:
+
+
+ground_truth = spectral.imshow(classes=y, figsize=(5, 5))
+# spectral.save_rgb("./result/ground_truth.png", y, format='png', colors=spectral.spy_colors)
+# In[17]:
+
+
+predict_image = spectral.imshow(classes=outputs.astype(int), figsize=(5, 5))
+# spectral.save_rgb("./result/predict_image.png", outputs, format='png', colors=spectral.spy_colors)
---
+++ train.py
@@ -0,0 +1,106 @@
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import scipy
+import os
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras import backend as K
+
+K.set_image_dim_ordering('th')
+from keras.utils import np_utils
+
+# from sklearn.cross_validation import StratifiedKFold
+
+
+# In[2]:
+
+
+PATH = os.getcwd()
+print(PATH)
+
+# In[3]:
+
+
+# Global Variables
+windowSize = 5
+numPCAcomponents = 30
+testRatio = 0.25
+
+# # Load Training Dataset
+
+# In[4]:
+
+
+X_train = np.load("./data/XtrainWindowSize"
+                  + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(testRatio) + ".npy")
+
+y_train = np.load("./data/ytrainWindowSize"
+                  + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(testRatio) + ".npy")
+
+# In[5]:
+
+
+# Reshape into (number of samples, channels, height, width)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[3], X_train.shape[1], X_train.shape[2]))
+
+# In[6]:
+
+
+# convert class labels to on-hot encoding
+y_train = np_utils.to_categorical(y_train)
+
+# In[7]:
+
+
+# Define the input shape 
+input_shape = X_train[0].shape
+print(input_shape)
+
+# In[8]:
+
+
+# number of filters
+C1 = 3 * numPCAcomponents
+
+# In[9]:
+
+
+# Define the model
+model = Sequential()
+
+model.add(Conv2D(C1, (3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(3 * C1, (3, 3), activation='relu'))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(6 * numPCAcomponents, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(16, activation='softmax'))
+
+# In[10]:
+
+
+sgd = SGD(lr=0.0001, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# In[14]:
+
+
+model.fit(X_train, y_train, batch_size=32, epochs=30)
+
+# In[15]:
+
+
+import h5py
+from keras.models import load_model
+
+# In[17]:
+
+
+model.save('./model/my_model_epochs30.h5')
